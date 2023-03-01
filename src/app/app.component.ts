783f75fe4573b31e9f02e357a671c48bb2496c5a
tarasHorodyskyi <horodyskyi777@icloud.com>
2023-03-01 10:48:31 +0200

    test task for Usense

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'testForUsense';
+  loginForm!: FormGroup;
+  colors!:string[];
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+   this.initForm()
+   this.checkPasswordStrength();
+  }
+
+  initForm():void {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+  
+  checkPasswordStrength():void {
+    this.password?.valueChanges.subscribe(value => {
+      if (!value) {
+        this.colors = ['lightgray', 'lightgray', 'lightgray'];
+      } else if (value && value.length < 8) {
+        this.colors = ['red', 'lightgray', 'lightgray'];
+      } else if (/^[a-zA-Z]+$/.test(value) || /^[0-9]+$/.test(value)) {
+        this.colors = ['red', 'lightgray', 'lightgray'];
+      } else if (/^[a-zA-Z0-9]+$/.test(value)) {
+        this.colors = ['yellow', 'yellow', 'gray'];
+      } else {
+        this.colors = ['limegreen', 'limegreen', 'limegreen'];
+      }
+    })
+  }
+
 }
